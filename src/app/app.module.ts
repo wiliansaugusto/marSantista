08683e53fc339435d3b7b549200d5effa9fd5c92
wiliansaugusto <wiliansaugusto@gmail.com>
2023-04-18 15:38:55 -0300

    Primeiro Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './views/home/home.component';
+import {MatExpansionModule} from '@angular/material/expansion'; 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/views/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  providers: [NgbCarouselConfig], // add NgbCarouselConfig to the component providers
+})
+export class HomeComponent {
+  showNavigationArrows = false;
+	showNavigationIndicators = false;
+	images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  panelOpenState= false;
+
+  constructor(config: NgbCarouselConfig) {
+		// customize default values of carousels used by this component tree
+		config.showNavigationArrows = true;
+		config.showNavigationIndicators = true;
+	}
+}
